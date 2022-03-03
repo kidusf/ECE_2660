046518cc22583d5f929cc7467680fd39d28b63ae
kidusf <kidusfasil@gmail.com>
2022-03-03 15:06:46 -0500

    added labels and titles

--- ECE_2660/3.2/ft.py
+++ ECE_2660/3.2/ft.py
@@ -7,6 +7,10 @@ multisim_data.columns=["time", "voltage"]
 t=multisim_data["time"]
 w_o=2000*np.pi
 y=0.807255*np.cos(w_o*t-np.pi/180*23.8051)
-plt.plot(t, y)
-plt.plot(t, multisim_data["voltage"])
+plt.plot(t, y, color="red", linewidth=2, linestyle="dashed")
+plt.plot(t, multisim_data["voltage"], color="blue")
+plt.xlabel("Time, t(seconds)")
+plt.ylabel("Voltage, V(volts")
+plt.title("Transient Response of the RL Filter")
+plt.legend(["Analytical", "Numerical"])
 plt.show()
