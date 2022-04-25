57bbeade9422314af8c101c4653ea4607a95a6c1
unknown <kidusfasil@gmail.com>
2022-04-25 02:47:57 -0400

    modified the script to have the amplitude of the triangle wave be roughly 1

--- 7.1/pwm.py
+++ 7.1/pwm.py
@@ -1,5 +1,9 @@
+#Kidus Fasil
+#kf5fr
+#Module 7
 import numpy as np
 
+#values in lab kit
 resistors_in_kit=np.array([10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82, 100, 120, 150, 180, 220, 270, 330, 470, 560, 
 680, 820, 1000, 1200, 1500, 1800, 2200, 2700, 3300, 4700, 5600, 6800, 8200, 12000, 15000, 
 18000, 22000, 27000, 33000, 47000, 56000, 68000, 82000, 120000, 150000,
@@ -11,13 +15,18 @@ r2=resistors_in_kit
 r3=resistors_in_kit
 c1=capacitors_in_kit
 
+#the threshold for the triangle wave frequency
 freq_threshold=1000
+#the tolerance for the frequency
 tolerance=100
+#the tolerance for the amplitude
+amp_tolerance=0.05
 
+#go through every possible combination and see which ones satisfy the requirements
 for i in range(len(r1)):
     for j in range(len(r2)):
         for k in range(len(r3)):
             for l in range(len(c1)):
                 actual_freq=(1/(4*r1[i]*c1[l]))*r3[k]/r2[j]
-                if((actual_freq-freq_threshold)<=tolerance and actual_freq-freq_threshold>=0 and r2[j]/r3[k]>=1):
-                    print(r1[i], r2[j], r3[k], c1[l], actual_freq, r2[j]/r3[k])
+                if((actual_freq-freq_threshold)<=tolerance and actual_freq-freq_threshold>=0 and abs(r2[j]/r3[k]-0.2)<=amp_tolerance):
+                    print(r1[i], r2[j], r3[k], c1[l], actual_freq, (r2[j]/r3[k])*5)
